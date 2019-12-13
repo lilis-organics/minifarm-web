4695f893b62be36745bd01a62a75f284dcbb9cbc
yanfeng <yanfeng.me@gmail.com>
2019-12-12 23:52:02 -0500

    upgrade nextjs to 9.1.5

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,20 @@
 import Layout from '../components/layout';
+import PropTypes from 'prop-types';
 
-const About = () => (
+const About = props => (
   <Layout>
-    <p>Welcome to about page! ver. 0.1.1</p>
+    <p>Welcome to about page! ver. {props.version}</p>
   </Layout>
 );
+
+About.getInitialProps = async () => {
+  return {
+    version: '0.1.3'
+  };
+};
+
+About.propTypes = {
+  version: PropTypes.string
+};
 
 export default About;
